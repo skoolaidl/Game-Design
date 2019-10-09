14a5935a505d3cb86641559e367e79f57fb5cfbf
Carter Greis <55456647+carter-greis@users.noreply.github.com>
2019-10-09 12:38:02 -0400

    Minor edits
    Changed RenderWindow name to Match Made in Hell and added res folder

--- bin/main.cpp
+++ bin/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv)
 {
   // create main window
-  sf::RenderWindow App(sf::VideoMode(800,600,32), "Hello World - SFML");
+  sf::RenderWindow App(sf::VideoMode(800,600,32), "Match Made in Hell");
 
   // start main loop
   while(App.isOpen())
