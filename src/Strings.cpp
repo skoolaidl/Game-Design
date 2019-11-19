e0d56bf807f9f881395b438fc87481e0556a4d3c
Carter Greis <55456647+carter-greis@users.noreply.github.com>
2019-11-19 11:10:21 -0500

    Fixed keys mapping in Strings

--- src/Strings.cpp
+++ src/Strings.cpp
@@ -77,12 +77,12 @@ Strings::Strings() {
     tierList.push_back("Satan’s Wingman");
     tierList.push_back("King of the Underworld");
 
-    keys = { "A","B","C","D","E","F","G","H","I","J","K",
+    keys = {"A","B","C","D","E","F","G","H","I","J","K",
         "L","M","N","O",
         "P","Q","R","S",
         "T","U","V","W",
         "X","Y","Z","Num0",
-        "Num1,""Num2,""Num3","Num4",
+        "Num1","Num2","Num3","Num4",
         "Num5","Num6","Num7","Num8",
         "Num9","Escape","LControl","LShift",
         "LAlt","LSystem","RControl","RShift",
